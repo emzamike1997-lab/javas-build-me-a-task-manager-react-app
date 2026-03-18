b912e073c35e1e91b34f522187f47fc546dc067a
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-18 13:05:30 +0100

    JAVAS: Add src/components/TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 
 function TaskList({ tasks, deleteTask }) {
-    return (
-        <div className="task-list">
-            <h2>Tasks</h2>
-            <ul>
-                {tasks.map((task) => (
-                    <li key={task.id}>
-                        <span>{task.title}</span>
-                        <button onClick={() => deleteTask(task.id)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <span>{task.title}</span>
+          <button onClick={() => deleteTask(task.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default TaskList
