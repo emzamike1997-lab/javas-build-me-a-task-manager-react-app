e99f460f699493e259eb6aae29657accced658f2
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-18 12:54:36 +0100

    JAVAS: Add src/components/TaskList.js

---
+++ src/components/TaskList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Task from './Task';
+
+function TaskList() {
+  return (
+    <div>
+      <h2>Task List</h2>
+      <Task />
+    </div>
+  );
+}
+
+export default TaskList;
+
+This is the basic structure for the task manager React app. We will continue to build on this structure in the next stages.
