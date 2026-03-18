7df701af733a60a9b7d62af0bec7158d54b5f55b
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-18 13:05:27 +0100

    JAVAS: Add src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,37 @@ import TaskList from './components/TaskList'
 import TaskForm from './components/TaskForm'
 
 function App() {
-    const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([])
 
-    useEffect(() => {
-        const storedTasks = localStorage.getItem('tasks')
-        if (storedTasks) {
-            setTasks(JSON.parse(storedTasks))
-        }
-    }, [])
-
-    const addTask = (task) => {
-        setTasks([...tasks, task])
-        localStorage.setItem('tasks', JSON.stringify([...tasks, task]))
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks')
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks))
     }
+  }, [])
 
-    const deleteTask = (id) => {
-        setTasks(tasks.filter((task) => task.id !== id))
-        localStorage.setItem('tasks', JSON.stringify(tasks.filter((task) => task.id !== id)))
-    }
+  const addTask = (task) => {
+    setTasks([...tasks, task])
+    localStorage.setItem('tasks', JSON.stringify([...tasks, task]))
+  }
 
-    return (
-        <div className="app">
-            <h1>Task Manager</h1>
-            <Link to="/tasks">Tasks</Link>
-            <Link to="/add-task">Add Task</Link>
-            <Routes>
-                <Route path="/tasks" element={<TaskList tasks={tasks} deleteTask={deleteTask} />} />
-                <Route path="/add-task" element={<TaskForm addTask={addTask} />} />
-            </Routes>
-        </div>
-    )
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+    localStorage.setItem('tasks', JSON.stringify(tasks.filter((task) => task.id !== id)))
+  }
+
+  return (
+    <div className="app">
+      <nav>
+        <Link to="/">Task List</Link>
+        <Link to="/add-task">Add Task</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<TaskList tasks={tasks} deleteTask={deleteTask} />} />
+        <Route path="/add-task" element={<TaskForm addTask={addTask} />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
