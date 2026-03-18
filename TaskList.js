d9f25f07b02dd094805d1418011ae0542d9d1bb9
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-18 13:05:55 +0100

    JAVAS: Add TaskList.js

--- TaskList.js
+++ TaskList.js
@@ -1,4 +1,4 @@
-This component will render a list of all tasks.
+This component will render a list of tasks. It will receive the tasks and a deleteTask function as props.
 
 ```javascript
 import React from 'react';
@@ -6,10 +6,10 @@ import React from 'react';
 function TaskList({ tasks, deleteTask }) {
   return (
     <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>
-          {task.task}
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
+      {tasks.map((task, index) => (
+        <li key={index}>
+          {task}
+          <button onClick={() => deleteTask(index)}>Delete</button>
         </li>
       ))}
     </ul>
