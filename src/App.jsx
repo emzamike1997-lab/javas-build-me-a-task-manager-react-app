7c58a913c3b18bf097047d45c5176096b66d22b8
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-18 12:54:15 +0100

    JAVAS: Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import TaskList from './components/TaskList'
+import TaskForm from './components/TaskForm'
+
+function App() {
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        const storedTasks = localStorage.getItem('tasks')
+        if (storedTasks) {
+            setTasks(JSON.parse(storedTasks))
+        }
+    }, [])
+
+    const addTask = (task) => {
+        setTasks([...tasks, task])
+        localStorage.setItem('tasks', JSON.stringify([...tasks, task]))
+    }
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id))
+        localStorage.setItem('tasks', JSON.stringify(tasks.filter((task) => task.id !== id)))
+    }
+
+    return (
+        <div className="app">
+            <h1>Task Manager</h1>
+            <Link to="/tasks">Tasks</Link>
+            <Link to="/add-task">Add Task</Link>
+            <Routes>
+                <Route path="/tasks" element={<TaskList tasks={tasks} deleteTask={deleteTask} />} />
+                <Route path="/add-task" element={<TaskForm addTask={addTask} />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App
